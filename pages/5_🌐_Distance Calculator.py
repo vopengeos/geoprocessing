e2e3a48c39bb59966934d696cc9223ace5a26d72
Quach Dong Thang <quachdongthang@gmail.com>
2023-10-13 17:13:24 +0700

    Update 5_🌐_Distance Calculator.py

--- pages/5_🌐_Distance Calculator.py
+++ pages/5_🌐_Distance Calculator.py
@@ -1,4 +1,4 @@
-import folium
+import folium, os
 from folium.plugins import Draw, LocateControl
 import streamlit as st
 from streamlit_folium import st_folium, folium_static
@@ -13,6 +13,7 @@ import geopandas as gdp
 from shapely.geometry import Point, LineString
 from folium.plugins import Fullscreen
 import streamlit_ext as ste
+import geopandas as gpd
 # from pykalman import KalmanFilter
 import numpy as np
 from math import radians, cos, acos, sin, asin, sqrt
@@ -34,11 +35,15 @@ st.sidebar.info(
 st.title("Distance Calculator")
 st.write('Distance Calculator for GPS Track Logs')
 start_time = '2023-01-01 00:00:00'
-# start_time = '2019-01-01 00:00:00'
+start_time = '2019-01-01 00:00:00'
 end_time = '2023-12-31 00:00:00'
 MAX_ALLOWED_TIME_GAP = 300  # seconds
 MAX_ALLOWED_DISTANCE_GAP = 10000000000000  # meters
 col1, col2 = st.columns(2)
+
+shortestpath_geometries = []
+dict_ = {"time": [], "distance": [], "duration": [], "speed": []}
+
 
 def style_function(feature):
     return {
@@ -48,6 +53,16 @@ def style_function(feature):
         'weight': 2,
         # 'dashArray': '5, 5'
     }
+
+def style_function2(feature):
+    return {
+        'fillColor': '#b1dda3',
+        'fillOpacity': 0.5,
+        'color': 'red',
+        'weight': 2,
+        # 'dashArray': '5, 5'
+    }
+
 
 def highlight_function(feature):   
     return {
@@ -129,7 +144,7 @@ def removejumping(data):
         distance_diff = geopy.distance.geodesic((data.iloc[i-1].latitude, data.iloc[i-1].longitude), (data.iloc[i].latitude, data.iloc[i].longitude)).m
         # distance_diff = haversine(data.iloc[i-1].latitude, data.iloc[i-1].longitude, data.iloc[i].latitude, data.iloc[i].longitude)
         if time_diff > 0:
-            velocity =  (distance_diff*0.001)/(time_diff/3600) #km/h   
+            velocity =  (distance_diff/1000)/(time_diff/3600) #km/h   
             # st.write(data.iloc[i-1].datetime, data.iloc[i].datetime,velocity,' km/h') 
             if velocity > 70 and i< len(filtered) -1: #km/h, except final jumping point! Ex: WayPoint_20230928142338.csv        
                 # filtered = filtered.drop([i])
@@ -181,43 +196,58 @@ def traveledDistance(data):
     shortestpath_distance = []
     crowfly_distance = []
     for i in range (1, len(data)):
+        velocity = 0
         time_diff = (datetime.strptime(str(data.iloc[i].datetime), '%Y-%m-%d %H:%M:%S') - datetime.strptime(str(data.iloc[i - 1].datetime), '%Y-%m-%d %H:%M:%S')).total_seconds()
         # distance_temp = greatCircle(data.iloc[i].longitude, data.iloc[i].latitude, data.iloc[i - 1].longitude, data.iloc[i - 1].latitude)
         # distance_temp = haversine((data.iloc[i-1].latitude, data.iloc[i-1].longitude), (data.iloc[i].latitude, data.iloc[i].longitude)).m
         distance_temp = geopy.distance.geodesic((data.iloc[i-1].latitude, data.iloc[i-1].longitude), (data.iloc[i].latitude, data.iloc[i].longitude)).m
-        
-        velocity =  (distance_temp*0.001)/(time_diff/3600) #km/h      
+        if time_diff>0:
+            velocity =  (distance_temp/1000)/(time_diff/3600) #km/h      
         # # if time_diff > MAX_ALLOWED_TIME_GAP or distance_temp > MAX_ALLOWED_DISTANCE_GAP:
         #     # #distance_temp = 0
         #     # st.write(data.iloc[i].datetime)        
         try:  
-            if velocity > 70 or time_diff > MAX_ALLOWED_TIME_GAP : #km/h , MAX_ALLOWED_TIME_GAP = 300s in case of GPS signals lost for more than MAX_ALLOWED_TIME_GAP seconds
+            if velocity > 70 or time_diff > MAX_ALLOWED_TIME_GAP or distance_temp > 200:  #km/h , MAX_ALLOWED_TIME_GAP = 300s in case of GPS signals lost for more than MAX_ALLOWED_TIME_GAP seconds
+                st.write(data.iloc[i-1].datetime)
+                st.write(data.iloc[i].datetime)
+                st.write('velocity: ',  velocity)
+                st.write('time_diff: ', time_diff)
+                st.write('distance_temp:', distance_temp)
                 coor = [[data.iloc[i - 1].longitude, data.iloc[i - 1].latitude], [data.iloc[i].longitude, data.iloc[i].latitude]]
-                api = OSRM(base_url="https://routing.openstreetmap.de/routed-car/")
+                api = OSRM(base_url="https://routing.openstreetmap.de/routed-foot/")
                 # print(data.iloc[i - 1].longitude, data.iloc[i - 1].latitude, data.iloc[i].longitude, data.iloc[i].latitude, )
                 route = api.directions(
                 profile='car',
                 locations= coor       
                 )
+                dict_["time"].append(data.iloc[i - 1].datetime)
+                dict_["distance"].append(route.distance)
+                dict_["duration"].append(route.duration)
+                if (route.duration > 0):
+                    dict_["speed"].append((route.distance/1000)/(route.duration/3600)) # km/h
+                else: dict_["speed"].append(0)
+
                 crowfly_distance.append(round(distance_temp,2))
-                distance_temp = route.distance
+                distance_temp = route.distance                
                 shortestpath_distance.append(round(route.distance,2))
                 shortestpath_index.append(data.iloc[i-1].datetime)  
                 shortestpath_index.append(data.iloc[i].datetime)  
+                
+                shortestpath_geometries.append(LineString(route.geometry))
                 count += 1
         except:
             pass
         # Access the route properties with .geometry, .duration, .distance                  
         # print("Loop:", i, "timediff:", time_diff, "Distance Temp:", distance_temp, "Motion Activity:", data.iloc[i].motionActivity)
         totalDistance += distance_temp
-    st.write('Number of using shortest path in distance calculation: ', count, shortestpath_index,'Crow fly distance: ' , crowfly_distance, 'Shortest Path: ', shortestpath_distance)
+    st.write('Number of using shortest path in distance calculation: ', count, shortestpath_index,'Crow fly distance: ' , crowfly_distance, 'Shortest Path Distance: ', shortestpath_distance)
     return round(totalDistance/1000, 3)
 
 def download_geojson(gdf, layer_name):
     if not gdf.empty:        
         geojson = gdf.to_json()  
         ste.download_button(
-            label="Download " +  layer_name,
+            label="Download " + layer_name,
             file_name= layer_name+ '.geojson',
             mime="application/json",
             data=geojson
@@ -225,6 +255,7 @@ def download_geojson(gdf, layer_name):
 
 with col1:
     form = st.form(key="distance_calculator")
+    layer_name = 'track'
     with form: 
         url = st.text_input(
                 "Enter a CSV URL with Latitude and Longitude Columns",
@@ -232,10 +263,13 @@ with col1:
             )
         uploaded_file = st.file_uploader("Or upload a CSV file with Latitude and Longitude Columns")
         lat_column_index, lon_column_index = 0,0     
+
         if url:   
-            df = pd.read_csv(url,encoding = "UTF-8")                
+            df = pd.read_csv(url,encoding = "UTF-8")    
+            layer_name = url.split("/")[-1].split(".")[0]            
         if uploaded_file:        
             df = pd.read_csv(uploaded_file,encoding = "UTF-8")
+            layer_name = os.path.splitext(uploaded_file.name)[0]
         m = folium.Map(max_zoom = 21,
                     tiles='stamenterrain',
                     zoom_start=14,
@@ -259,7 +293,7 @@ with col1:
         folium_static(m, width = 600)
         geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
         trackpoints_origin = gdp.GeoDataFrame(df, geometry=geometry, crs = 'epsg:4326')        
-        download_geojson(trackpoints_origin, 'original track points')
+        download_geojson(trackpoints_origin,layer_name)
         submitted = st.form_submit_button("Calculate Distance")    
     
 
@@ -318,7 +352,7 @@ if submitted:
                         highlight_function=highlight_function,
                         marker = folium.Marker(icon=folium.Icon(
                                     icon='ok-circle',
-                                    color = 'purple',
+                                    color = 'green',
                                     size = 5
                                     )),     
                         # marker =  folium.CircleMarker(fill=True),
@@ -335,8 +369,18 @@ if submitted:
                         # )
                         ).add_to(m)
 
+        routes_df = gpd.GeoDataFrame(dict_, geometry=shortestpath_geometries, crs="EPSG:4326")
+        folium.GeoJson(routes_df, name = 'shortest_path',  
+                        style_function = style_function2, 
+                        highlight_function=highlight_function,
+                        # popup = folium.GeoJsonPopup(
+                        # fields = tracpoints_cleaned_fields
+                        # )
+                        ).add_to(m)
+        
         m.fit_bounds(m.get_bounds(), padding=(30, 30))
         folium_static(m, width = 600)
-        download_geojson(trackpoints_cleaned, 'track_points_cleaned')
-        download_geojson(track_distance, 'track')
+        download_geojson(trackpoints_cleaned, layer_name + '_cleaned')
+        download_geojson(track_distance, layer_name + '_track')
+        download_geojson(routes_df, layer_name + '_shortestpath')
 
